6e9b12f1878c5b4ed786a82e2ca056266d232c02
0806jisu <0806jisu@skola.falkoping.se>
2025-09-25 08:41:54 +0000

    done

--- Program.cs
+++ Program.cs
@@ -1,87 +1,89 @@
-﻿Console.WriteLine("Välkommen till mitt program");
-Console.WriteLine("Vad heter du");
+﻿// Console.WriteLine("Välkommen till mitt program");
+// Console.WriteLine("Vad heter du");
 
-string namn = Console.ReadLine();
-Console.WriteLine($"Hej {namn}, hur gammal är du?");
+// string namn = Console.ReadLine();
+// Console.WriteLine($"Hej {namn}, hur gammal är du?");
 
-string alder = Console.ReadLine();
-int age = Convert.ToInt32(alder);
-Console.WriteLine($"Om ett år är du {age + 1} år gammal");
-if (age > 15)
-{
-    Console.WriteLine("Du är fortfarande ung");
-}
-else
-{
-    Console.WriteLine("Du är vuxen");
-}
-Console.WriteLine("Vad heter huvudstaden i Sverige?");
-string huvudstad = Console.ReadLine();
-if (huvudstad == "Stockholm")
-{
-    Console.WriteLine("Rätt svar");
-}
-else
-{
-    Console.WriteLine("Fel svar");
-}
+// string alder = Console.ReadLine();
+// int age = Convert.ToInt32(alder);
+// Console.WriteLine($"Om ett år är du {age + 1} år gammal");
+// if (age > 15)
+// {
+//     Console.WriteLine("Du är fortfarande ung");
+// }
+// else
+// {
+//     Console.WriteLine("Du är vuxen");
+// }
+// Console.WriteLine("Vad heter huvudstaden i Sverige?");
+// string huvudstad = Console.ReadLine();
+// if (huvudstad == "Stockholm")
+// {
+//     Console.WriteLine("Rätt svar");
+// }
+// else
+// {
+//     Console.WriteLine("Fel svar");
+// }
 
-Console.WriteLine("Nästa fråga");
-Console.WriteLine("Hur många invånare har Sverige?");
-if (Console.ReadLine() == "10 miljoner")
-{
-    Console.WriteLine("Rätt svar");
-}
-else
-{
-    Console.WriteLine("Fel svar");
-}
-Console.WriteLine("Vem vann fotbolls-VM 2018?");
-if (Console.ReadLine() == "Frankrike")
-{
-    Console.WriteLine("Rätt svar");
-}
-else
-{
-    Console.WriteLine("Fel svar");
-}
-Console.WriteLine("Vill du fortsätta?");
-if (Console.ReadLine() == "Ja")
-{
-    // Fortsätt med programmet
-}
-else
-{
-    Console.WriteLine("Tack för att du deltog!");
-    Environment.Exit(0);
-}
-// Fortsätt med programmet
-    Console.WriteLine("Hur många länder finns det i Europa?");
-    if (Console.ReadLine() == "44")
-    {
-        Console.WriteLine("Rätt svar");
-    }
-    else
-    {
-        Console.WriteLine("Fel svar");
-    }
-    Console.WriteLine("Vilket år startade andra världskriget?");
-    if (Console.ReadLine() == "1939")
-    {
-        Console.WriteLine("Rätt svar");
-    }
-    else
-    {
-        Console.WriteLine("Fel svar");
-    }
+// Console.WriteLine("Nästa fråga");
+// Console.WriteLine("Hur många invånare har Sverige?");
+// if (Console.ReadLine() == "10 miljoner")
+// {
+//     Console.WriteLine("Rätt svar");
+// }
+// else
+// {
+//     Console.WriteLine("Fel svar");
+// }
+// Console.WriteLine("Vem vann fotbolls-VM 2018?");
+// if (Console.ReadLine() == "Frankrike")
+// {
+//     Console.WriteLine("Rätt svar");
+// }
+// else
+// {
+//     Console.WriteLine("Fel svar");
+// }
+// Console.WriteLine("Vill du fortsätta?");
+// if (Console.ReadLine() == "Ja")
+// {
+//     // Fortsätt med programmet
+// }
+// else
+// {
+//     Console.WriteLine("Tack för att du deltog!");
+//     Environment.Exit(0);
+// }
+// // Fortsätt med programmet
+//     Console.WriteLine("Hur många länder finns det i Europa?");
+//     if (Console.ReadLine() == "44")
+//     {
+//         Console.WriteLine("Rätt svar");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Fel svar");
+//     }
+//     Console.WriteLine("Vilket år startade andra världskriget?");
+//     if (Console.ReadLine() == "1939")
+//     {
+//         Console.WriteLine("Rätt svar");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Fel svar");
+//     }
 
-    Console.WriteLine("Tack att du deltog i frågesporten");
-    Console.WriteLine("Vill du avsluta programmet?");
-    if (Console.ReadLine() == "Ja")
-    {
-        Console.WriteLine("Programmet avslutas");
-    }
-    else
-    {
-        Console.WriteLine("slut på frågor");
-    }
+//     Console.WriteLine("Tack att du deltog i frågesporten");
+//     Console.WriteLine("Vill du avsluta programmet?");
+//     if (Console.ReadLine() == "Ja")
+//     {
+//         Console.WriteLine("Programmet avslutas");
+//     }
+//     else
+//     {
+//         Console.WriteLine("slut på frågor");
+//     }
+
+
